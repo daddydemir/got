8c1d65b4393e4b9934c7cd4d674e69fdc3faa312
daddydemir <mehmetcakmaktasi47@gmail.com>
2023-02-05 15:21:37 +0300

    objectmodel added.

--- handlers/createModel.go
+++ handlers/createModel.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/daddydemir/got/core"
 	"github.com/daddydemir/got/models"
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,6 +10,7 @@ func createModelHandler(c *fiber.Ctx) error {
 	if c.Method() == "POST" {
 		var request models.ParseModel
 		_ = c.BodyParser(&request)
+		core.ParseWithJson(request.Data)
 		return c.Send([]byte(request.Data))
 	} else {
 		return c.SendString("get method")
---
+++ models/objectmodel.go
@@ -0,0 +1,6 @@
+package models
+
+type Object struct {
+	Name string
+	Type string
+}
